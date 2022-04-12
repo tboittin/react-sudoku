aa1aa53532e7e6d01e93b759a6a21f7150b20448
Thomas Boittin <thomasdavid.boittin@ausy.fr>
2022-04-12 10:28:49 +0200

    card component

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
 
-import { Content, Title } from './components'
+import { Card, Content, Title } from './components'
 import { unregister } from './core'
 import { GlobalStyles, theme } from './styles'
 
@@ -12,6 +12,9 @@ ReactDOM.render(
       <GlobalStyles />
       <Content data-cy="content">
         <Title data-cy="title">Sudoku</Title>
+        <Card data-cy="card">
+          <p>This is a work in progress.</p>
+        </Card>
       </Content>
     </ThemeProvider>
   </React.StrictMode>,
