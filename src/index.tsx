6c389d88a169556f9eaf428cc567bde0500dc065
Thomas Boittin <thomasdavid.boittin@ausy.fr>
2022-04-12 10:00:20 +0200

    tree and title and content components

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
+
+import { Content, Title } from './components'
+import { unregister } from './core'
+import { GlobalStyles, theme } from './styles'
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>Hello World</div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Content data-cy="content">
+        <Title data-cy="title">Sudoku</Title>
+      </Content>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+unregister()
