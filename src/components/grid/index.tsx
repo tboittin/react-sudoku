9e05d78f39b0be6c2ae669f8daf54dabdda3ac49
Thomas Boittin <thomasdavid.boittin@ausy.fr>
2022-04-12 15:33:24 +0200

    grid block

---
+++ src/components/grid/index.tsx
@@ -0,0 +1,24 @@
+import React, { FC, Children } from 'react'
+import Block from './block'
+
+import { Container, Row } from './styles'
+
+const Grid: FC = () => {
+  return (
+    <Container data-cy="grid-container">
+      {Children.toArray(
+        [...Array(9)].map((_, rowIndex) => (
+          <Row data-cy="grid-row-container">
+            {Children.toArray(
+              [...Array(9)].map((_, colIndex) => (
+                <Block colIndex={colIndex} rowIndex={rowIndex} />
+              ))
+            )}
+          </Row>
+        ))
+      )}
+    </Container>
+  )
+}
+
+export default Grid
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
 
-import { Card, Content, Title } from './components'
+import { Card, Content, Title, Grid } from './components'
 import { unregister } from './core'
 import { GlobalStyles, theme } from './styles'
 
@@ -13,7 +13,7 @@ ReactDOM.render(
       <Content data-cy="content">
         <Title data-cy="title">Sudoku</Title>
         <Card data-cy="card">
-          <p>This is a work in progress.</p>
+          <Grid />
         </Card>
       </Content>
     </ThemeProvider>
