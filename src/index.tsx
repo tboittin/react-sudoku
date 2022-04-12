7fb2ab8d5c94630c91d7e48b22406731010aacfb
Thomas Boittin <thomasdavid.boittin@ausy.fr>
2022-04-12 16:18:00 +0200

    Absolute Imports and Types

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
 
-import { Card, Content, Title, Grid } from './components'
-import { unregister } from './core'
-import { GlobalStyles, theme } from './styles'
+import { Card, Content, Title, Grid } from 'components'
+import { unregister } from 'core'
+import { GlobalStyles, theme } from 'styles'
 
 ReactDOM.render(
   <React.StrictMode>
