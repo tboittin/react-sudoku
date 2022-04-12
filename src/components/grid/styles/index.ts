fa0598ed7915bf361311d1b1e3fcaccaa207e189
Thomas Boittin <thomasdavid.boittin@ausy.fr>
2022-04-12 15:56:36 +0200

    grid border

--- src/components/grid/styles/index.ts
+++ src/components/grid/styles/index.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
@@ -6,6 +6,40 @@ export const Container = styled.div`
   margin: 10px 0;
 `
 export const Row = styled.div`
-  display: flex;
-  flex-flow: row;
+  ${({ theme }) => css`
+    display: flex;
+    flex-flow: row;
+
+    &:nth-child(1) {
+      div {
+        border-top: solid 4px ${theme.colors.black};
+      }
+    }
+    &:nth-child(3),
+    &:nth-child(6) {
+      div {
+        border-bottom: solid 3px ${theme.colors.black};
+      }
+    }
+    &:nth-child(9) {
+      div {
+        border-bottom: solid 4px ${theme.colors.black};
+      }
+    }
+
+    div {
+      &:nth-child(1) {
+        border-left: solid 4px ${theme.colors.black};
+      }
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9) {
+        border-right: solid 4px ${theme.colors.black};
+      }
+      &:nth-child(4),
+      &:nth-child(7) {
+        border-left: none;
+      }
+    }
+  `}
 `
