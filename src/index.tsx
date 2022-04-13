d03a7f6a3bd31a51642476e570a9cb0a45545ec6
Thomas Boittin <thomasdavid.boittin@ausy.fr>
2022-04-13 15:50:33 +0200

    Setting up Redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,24 @@ import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
 
 import { Card, Content, Title, Grid } from 'components'
-import { unregister } from 'core'
+import { configureStore, unregister } from 'core'
 import { GlobalStyles, theme } from 'styles'
+import { Provider } from 'react-redux'
+
+const store = configureStore()
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Content data-cy="content">
-        <Title data-cy="title">Sudoku</Title>
-        <Card data-cy="card">
-          <Grid />
-        </Card>
-      </Content>
+      <Provider store={store}>
+        <Content data-cy="content">
+          <Title data-cy="title">Sudoku</Title>
+          <Card data-cy="card">
+            <Grid />
+          </Card>
+        </Content>
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
