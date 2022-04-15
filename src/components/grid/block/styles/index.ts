8ea11073c8a0469ebf97d107644328ecc105f7ff
Thomas Boittin <thomasdavid.boittin@ausy.fr>
2022-04-15 17:11:40 +0200

    Styled Buttons

--- src/components/grid/block/styles/index.ts
+++ src/components/grid/block/styles/index.ts
@@ -2,19 +2,20 @@ import styled, { css } from 'styled-components'
 
 interface IProps {
   active?: boolean
+  puzzle?: boolean
 }
 
 export const Container = styled.div<IProps>`
-  ${({ active, theme }) => css`
+  ${({ puzzle, active, theme }) => css`
     align-items: center;
-    background-color: ${active? theme.colors.blue : theme.colors.white};
+    background-color: ${active ? theme.colors.blue : theme.colors.white};
     border: 1px solid ${theme.colors.black};
     cursor: pointer;
     display: flex;
     flex-grow: 1;
     flex-shrink: 0;
     flex-basis: 0;
-    font-weight: bold;
+    font-weight: ${puzzle ? 'bold' : 'normal'};
     font-size: 20px;
     height: auto;
     justify-content: center;
