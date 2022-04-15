4f851318de12bd6210c61a2917e16657a329b2dd
Thomas Boittin <thomasdavid.boittin@ausy.fr>
2022-04-15 17:56:11 +0200

    Button Mapping

--- src/components/numbers/button/index.tsx
+++ src/components/numbers/button/index.tsx
@@ -1,15 +1,37 @@
 import { Button } from 'components/styles'
-import { FC } from 'react'
-import { NUMBERS } from 'typings'
+import { Dispatch, FC, useCallback } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { IReducer } from 'reducers'
+import { fillBlock } from 'reducers/actions'
+import { AnyAction } from 'redux'
+import { BLOCK_COORDS, N, NUMBERS } from 'typings'
 
 interface IProps {
   value: NUMBERS
 }
 
+interface IState {
+  selectedBlock?: BLOCK_COORDS
+  selectedValue: N
+}
+
 const NumberButton: FC<IProps> = ({ value }) => {
-  function fill() {
-    console.log('fill', value)
-  }
+  const state = useSelector<IReducer, IState>(
+    ({ selectedBlock, workingGrid }) => ({
+      selectedBlock,
+      selectedValue:
+        workingGrid && selectedBlock
+          ? workingGrid[selectedBlock[0]][selectedBlock[1]]
+          : 0,
+    })
+  )
+  const dispatch = useDispatch<Dispatch<AnyAction>>()
+
+  const fill = useCallback(() => {
+    if (state.selectedBlock && state.selectedValue === 0) {
+      dispatch(fillBlock(value, state.selectedBlock))
+    }
+  }, [dispatch, state.selectedBlock, state.selectedValue, value])
   return <Button onClick={fill}>{value}</Button>
 }
 
